78f6d54fdaad37bbafb1b69bc7e5006b611fce9d
SangZi <zi.sang@hermesworld.com>
2023-04-05 14:45:49 +0200

    add method under the property methods, interpolation with methods

--- src/components/MySecondComponent.vue
+++ src/components/MySecondComponent.vue
@@ -1,21 +1,29 @@
 <template>
-  <h1>My Email is {{email}}</h1>
-  <h1>My Mobole is {{mobile}}</h1>
+  <h1>My Email is {{getContactInfo().email}}</h1>
+  <h1>My Mobile is {{getContactInfo().mobile}}</h1>
   <h1>My Name is {{getName("Zi")}}</h1>
 </template>
 
 <script>
   export default {
-    name: "MySecondCpmponent",
+    name: "MySecondComponent",
     data() {
       return {
         email: "zi.sang@testmail.de",
         mobile: "123456789",
-        getName(name) {
-          return name;
+
       }
+    },
+    methods: {
+      getName(name) {
+        return name;
+      },
+      getContactInfo() {
+        return {
+          email: this.email, //with "this.email", here it gets the value from data()
+          mobile: this.mobile
+        }
       }
     }
   }
-
 </script>
