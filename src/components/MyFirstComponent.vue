94bc306d01b66c16f1d4246aa3e155cbc1d9ca09
SangZi <zi.sang@hermesworld.com>
2023-04-05 11:16:14 +0200

    init with vue cli and add first component

---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>This is my first component!</h1>
+</template>
+
+<script>
+  export default {
+    name: "MyFirstComponent"
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: orange;
+  }
+</style>
