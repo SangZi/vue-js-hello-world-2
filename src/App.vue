07f28d07d27ff864da9fbed08ff8b12e959936c9
SangZi <zi.sang@hermesworld.com>
2023-04-07 12:46:43 +0200

    add my first event, interaction between component and method

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <MyFirstComponent></MyFirstComponent>
   <MySecondComponentForContact></MySecondComponentForContact>
+  <my-button></my-button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import MyFirstComponent from "@/components/MyFirstComponent.vue";
 import MySecondComponentForContact from "@/components/MySecondComponent.vue";
+import MyButton from "@/components/MyButton.vue";
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     MyFirstComponent,
-    MySecondComponentForContact
+    MySecondComponentForContact,
+    MyButton
   }
 }
 </script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- there is no difference between v-on:click and @click -->
+  <button @click="showAlertInfo()">Button1, click me</button>
+  <!-- you can pass some data to button -->
+  <!-- method param by calling method in component, it MUST BE IN SINGLE QUOTE, NOT DOUBLE QUOTE! -->
+  <button v-on:click ="showAlertInfoWithParam('Hello from Button2')">Button2, click me</button>
+  <!-- a button for double click -->
+  <button v-on:dblclick="showAlertInfo()">Button3, double click me</button>
+  <!-- a mousemove event -->
+  <h1 v-on:mousemove="showLogInfo()">Move you mouse over here</h1>
+  <!-- build a button4, and how many times it has been clicked show be shown below -->
+  <button @click="increaseCount()">Button4</button>
+  <h2>A counter, how many times i clicked Button4: {{count}}</h2>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    showAlertInfo() {
+      alert("some alert info");
+    },
+    showAlertInfoWithParam(greetingFromButton) {
+      alert("some alert info: " + greetingFromButton);
+    },
+    showLogInfo() {
+      console.log("some console log info");
+    },
+    increaseCount() {
+      //this key word "this", you cann in methods property access the field in data!
+      this.count++;
+    }
+  }
+}
+</script>
